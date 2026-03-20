b7405c814d662468a6972ba6200fed0fcec46d59
Naimul Islam <ms0413354@gmail.com>
2026-03-20 20:21:16 +0600

    add all nesserarry file and all things

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { config } from 'dotenv';
+import { errorMiddleWare } from './middlewares/errorHandler';
+
+export const app = express();
+config();
+
+app.use(
+	cors({
+		origin: [process.env.FRONTEND_URL],
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		credentials: true,
+	})
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', async (req, res) => {
+	const result = await pool.query('SELECT current_database()');
+	res.send(`The databse name is: ${result.rows[0].current_database}`);
+});
+
+app.use(errorMiddleWare);
